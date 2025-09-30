e66d97a0ff1ac50625a1660d9bc37a0a23e3de9e
messed up game <tom@gotbackuptomb.com>
2025-09-30 10:51:29 -0700

    Create App.jsx
    export default function App() {
      return (
        <div style={{ padding: 24 }}>
          <h1>Messed Up Game</h1>
          <p>It works! If you can see this on Netlify, the build & routing are good.</p>
        </div>
      );
    }

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Messed Up Game</h1>
+      <p>It works! If you can see this on Netlify, the build & routing are good.</p>
+    </div>
+  );
+}
