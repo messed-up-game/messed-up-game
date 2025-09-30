9b67adc4071bf541f0b0dbdfe1a98ad3826bf8a3
messed up game <tom@gotbackuptomb.com>
2025-09-29 18:33:02 -0700

    Update App.jsx
    src/App.jsx

--- exact names/src/App.jsx
+++ exact names/src/App.jsx
@@ -1,8 +1,1 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Messed Up Game</h1>
-      <p>It works! If you can see this on Netlify, the build & routing are good.</p>
-    </div>
-  );
-}
+src/App.jsx
