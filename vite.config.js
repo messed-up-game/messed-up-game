b59ef37bfe632d8c4a72b7e5c25b2065ed39fa1b
messed up game <tom@gotbackuptomb.com>
2025-10-15 10:07:29 -0700

    Simplify Vite configuration by removing build options

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [react()] })
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-})
 
