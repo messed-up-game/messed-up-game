bed1dcb9e3e0f685ac4dea93f1c22818fba9827d
messed up game <tom@gotbackuptomb.com>
2025-12-02 18:49:21 -0800

    Update comment for allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    // Allow the Netlify preview host for the Visual Editor
+    // Allow Netlify's devserver host for the Visual Editor
     allowedHosts: ["devserver-preview--messed-up-game-project.netlify.app"],
   },
 });
