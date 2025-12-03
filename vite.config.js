c6b9e734794cd421eb332d0fb8ed998a37671f8d
messed up game <tom@gotbackuptomb.com>
2025-12-02 18:54:44 -0800

    Fix formatting of allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    // Allow Netlify's devserver host for the Visual Editor
-    allowedHosts: ["devserver-preview--messed-up-game-project.netlify.app"],
- },
+    allowedHosts: [
+      "devserver-preview--messed-up-game-project.netlify.app"
+    ],
+  },
 });
+
